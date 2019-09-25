436034af8ccc07f095ea8886070a9dc18f6473f4
Pablo Quiceno <pabloquiceno95@gmail.com>
2019-09-25 15:48:05 -0500

    Refactor in get album

--- app/graphql/albums/resolvers.js
+++ app/graphql/albums/resolvers.js
@@ -1,9 +1,17 @@
 const album = require('../../services/album');
+const { getPhotosOfAlbum } = require('../../services/photos');
+const { getArtistByAlbum } = require('../../services/artists');
 
 const getAlbum = (_, params) => album.getAlbum(params);
+const getPhotos = ({ id }) => getPhotosOfAlbum(id);
+const getArtist = ({ id }) => getArtistByAlbum(id);
 
 module.exports = {
   Query: {
     album: getAlbum
+  },
+  Album: {
+    photos: getPhotos,
+    artist: getArtist
   }
 };
