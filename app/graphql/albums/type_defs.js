ad946d02e7492af29334e1c7ee372ad874a5c93d
Pablo Quiceno <pabloquiceno95@gmail.com>
2019-09-25 16:03:13 -0500

    Refactor in get album

--- app/graphql/albums/type_defs.js
+++ app/graphql/albums/type_defs.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server');
 const rootTypes = gql`
   extend type Query {
     album(id: ID!): Album
-    albums(offset: Int, limit: Int, orderBy: String): [Album]
+    albums(offset: Int = 1, limit: Int = 10, orderBy: String = "id"): [Album]
   }
 `;
 
